b3c356b7b3ca5fc75cdb8feca11f2fec5a01e339
Damian Farmer <damianfarmer7@gmail.com>
2021-07-11 00:53:02 -0400

    imports database

--- models.py
+++ models.py
@@ -1,4 +1,21 @@
 # create a database
+from sqlalchemy import create_engine,  Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///books.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = 'books'
+
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String)
+    author = Column('Author', String)
 # -->books.db
 # create a model
 # -->title, author, date published, price
